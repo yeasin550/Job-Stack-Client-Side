0be9d9ac7265fcaba902ae000f953ab499fb93d1
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-09-07 11:42:35 +0600

    Delete Message

--- src/API/ChatRequest.js
+++ src/API/ChatRequest.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://chat-app-project-server.vercel.app/'});
 
 export const createChat = (data) => API.post('/chat/', data);
 
 export const userChats = (id) => API.get(`/chat/${id}`);
+
+export const deleteChats = (id) => API.delete(`/chat/${id}`);
 
 export const findChat = (firstId, secondId) => API.get(`/chat/find/${firstId}/${secondId}`);
