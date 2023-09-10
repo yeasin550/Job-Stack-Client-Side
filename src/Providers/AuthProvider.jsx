855529f5ff72a18ae931110cafbb1b0d870a0522
yeasin550 <yeasin51570@gmail.com>
2023-09-10 14:47:40 +0600

    github sign in login

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import app from "../Firebase/firebase.config";
 import {
-    FacebookAuthProvider,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
     getAuth,
@@ -11,6 +10,7 @@ import {
     signOut,
     updateProfile,
     sendPasswordResetEmail,
+    GithubAuthProvider,
 } from "firebase/auth";
 
 //context api create
@@ -19,80 +19,76 @@ const auth = getAuth(app);
 
 // AuthProvider
 const AuthProvider = ({ children }) => {
-    //login user state
-    const [user, setUser] = useState(null);
-    // loding state
-    const [loading, setLoading] = useState(true);
-    // google provider & facebook provider
-    const googleProvider = new GoogleAuthProvider();
-    const facebookProvider = new FacebookAuthProvider();
-    // user create function firebase
-    const createUser = (email, password) => {
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
-    };
-    //sign in function website
-    const signIn = (email, password) => {
-        setLoading(true);
-        return signInWithEmailAndPassword(
-            auth,
-            email,
-            password,
-        );
-    };
-    //google sign in function
-    const googleSignIn = () => {
-        setLoading(true);
-        return signInWithPopup(auth, googleProvider);
-    };
-    // reset password
-      const resetPassword = (email) => {
-        return sendPasswordResetEmail(auth, email);
-      };
-    // facebook signin function
-    const facebookSighIn = () => {
-        setLoading(true);
-        return signInWithPopup(auth, facebookProvider)
-    }
+  //login user state
+  const [user, setUser] = useState(null);
+  // loding state
+  const [loading, setLoading] = useState(true);
+  // google provider & facebook provider
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+  // user create function firebase
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  //sign in function website
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  //google sign in function
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  // reset password
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+  // github sing in funtion
+  const gitHubSighIn = () => {
+      setLoading(true);
+      return signInWithPopup(auth, gitHubProvider)
+  }
 
-    //user logout 
-    const logOut = () => {
-        setLoading(true);
-        return signOut(auth);
-    };
+  //user logout
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    // user update profile
-    const updateUserProfile = (name, photo) => {
-        return updateProfile(auth.currentUser, {
-            displayName: name,
-            photoURL: photo,
-        });
-    };
+  // user update profile
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
-    //user login checke and set current user setuser state
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-        return () => {
-            return unsubscribe();
-        };
-    }, []);
-   
-    const authInfo = {
-      user,
-      loading,
-      createUser,
-      signIn,
-      googleSignIn,
-      logOut,
-      updateUserProfile,
-      facebookSighIn,
-      resetPassword,
+  //user login checke and set current user setuser state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      return unsubscribe();
     };
-    return (
-        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-    );
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    googleSignIn,
+    logOut,
+    updateUserProfile,
+    gitHubSighIn,
+    resetPassword,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
